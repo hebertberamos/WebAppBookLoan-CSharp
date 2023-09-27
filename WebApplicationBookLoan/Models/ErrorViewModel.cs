01260dabd72a4196f801799f48849884f9b459c1
hebert benigno <hebertramos09@gmail.com>
2023-09-27 11:49:10 -0300

    Project initialization

---
+++ WebApplicationBookLoan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationBookLoan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
